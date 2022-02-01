293928b721337df025b4b6766b48dce523f0acb9
sonia0409 <sonia.singh0409@gmail.com>
2022-02-01 12:26:39 -0500

    validating both the models: Store and Employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+    validates :first_name, :last_name, presence: true
+    validates :hourly_rate, numericality: { in: 40..20 }
+    validates :store, presence: true 
+end
+
+class Store
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+end
+
+
+puts "enter the store name"
+input_store_name = gets.chomp
+
+new_store = Store.create(
+    name: input_store_name
+)
+
+
+new_store.errors.each {|error, message|
+puts "#{error} #{message}"}
+
+
