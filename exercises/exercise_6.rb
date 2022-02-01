2474fd726de242950ff4a45205ad28f9bbe47028
sonia0409 <sonia.singh0409@gmail.com>
2022-02-01 12:24:56 -0500

    define relationship between Store model and Employee model

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+    has_many :employees
+end
+
+class Employee
+    belongs_to :store 
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Julia", last_name: "Chris", hourly_rate: 50)
+@store1.employees.create(first_name: "Christina", last_name: "Virani", hourly_rate: 20)
+
+@store2.employees.create(first_name: "Khushi", last_name: "Jain", hourly_rate: 30)
+@store2.employees.create(first_name: "Timmy", last_name: "Chen", hourly_rate: 45)
+@store2.employees.create(first_name: "Louis", last_name: "Lee", hourly_rate: 25)
